63f86272a17633d098481745c803d0cd3ceabed4
@bpreyer <b.a.preyerjr@gmail.com>
2021-05-27 20:59:23 -0400

    setting up the react

--- client/src/toDoList.js
+++ client/src/toDoList.js
@@ -1,1 +1,9 @@
-import React, {Component}
+import React, {Component} from "react"
+import axios from axios
+import { Card, Header, Form, Input, Icon } from "semantic-ui-react"
+
+let endpoint = "http://localhost:8080"
+
+class ToDoList extends Component {
+
+}
